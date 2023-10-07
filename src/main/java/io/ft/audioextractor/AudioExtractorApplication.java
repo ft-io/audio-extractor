f82799bf4911a57a8c52be8f4892575135e6ede7
유병우 <conner903@naver.com>
2023-10-07 15:18:21 +0900

    chore: spring 3.1.4 기반 프로젝트 세팅

---
+++ src/main/java/io/ft/audioextractor/AudioExtractorApplication.java
@@ -0,0 +1,13 @@
+package io.ft.audioextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ft/audioextractor/AudioExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ft.audioextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
